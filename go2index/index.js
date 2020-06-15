ede720e78aee8bd982239764816f3a32f5277d29
reddloob <46004053+reddloob@users.noreply.github.com>
2020-06-15 21:20:53 +0800

    Update index.js

--- go2index/index.js
+++ go2index/index.js
@@ -1,6 +1,6 @@
 // =======Options START=======
 var authConfig = {
-  siteName: "GoIndex-theme-acrou", // 网站名称
+  siteName: "", // 网站名称
   version: "1.1.1", // 程序版本
   theme: "acrou",
   // 强烈推荐使用自己的 client_id 和 client_secret
@@ -20,25 +20,15 @@ var authConfig = {
    * 不需要 Basic Auth 的盘，保持 user 和 pass 同时为空即可。（直接不设置也可以）
    * 【注意】对于id设置为为子文件夹id的盘将不支持搜索功能（不影响其他盘）。
    */
-  roots: [
-    {
-      id: "",
-      name: "TeamDrive",
-      pass: "",
-    },
-    {
-      id: "root",
-      name: "PrivateDrive",
-      user: "",
-      pass: "",
-      protect_file_link: true,
-    },
-    {
-      id: "",
-      name: "folder1",
-      pass: "",
-    },
-  ],
+  "roots": [
+	{
+		"id": "root",
+		"name": "",
+		"user": "",
+		"pass": "",
+		"protect_file_link": false
+	}
+],
   default_gd: 0,
   /**
    * 文件列表页面每页显示的数量。【推荐设置值为 100 到 1000 之间】；
@@ -69,7 +59,7 @@ var themeOptions = {
   // 主题版本号
   version: "2.0.5",
   //可选默认系统语言:en/zh-chs/zh-cht
-  languages: "en",
+  languages: "zh-chs",
   render: {
     /**
      * 是否渲染HEAD.md文件
